f91a5288f9f229803ac0733f02b636f9ddd5b63b
ysjang <ysjang@irush.co.kr>
2015-04-02 17:19:45 +0900

    test

--- springHello/src/main/java/com/kr/spring/HomeController.java
+++ springHello/src/main/java/com/kr/spring/HomeController.java
@@ -30,10 +30,12 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
+		String testText = "Success!!!!!!!!!!!!!";
 		
 		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("testText", testText );
 		
-		return "home";
+		return "test";
 	}
 	
 }
